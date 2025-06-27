4b91383009cfaa3a838f5c81fa0017c093ddb734
Trumptomz <gbadamosioluwatomiwa441@gmail.com>
2025-06-27 23:48:24 +0100

    restructured html

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 let allCountries = [];
-document.addEventListener("DOMContentLoaded", () => {
-
-});
 async function fetchCountries() {
    try {
       const response = await fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population");
